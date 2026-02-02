82f7043bced82362450a64e98546a02ca5eb6606
Sam Frauenhoffer <sammfrau@gmail.com>
2026-02-02 12:05:26 -0600

    Version 02/02/2026

---
+++ about.js
@@ -0,0 +1,50 @@
+// Accessing elements
+let menuDropdown = document.getElementById('menuDropdown');
+let menuOutputArea = document.getElementById('menuOutputArea');
+let homeButton = document.getElementById('homeButton');
+let aboutButton  = document.getElementById('aboutButton');
+
+// Defining elements
+let homeLink = 'index.html';
+let aboutLink = 'about.html';
+
+
+// Shows or hides an area when a button is clicked
+function toggleVisibility(element)
+{
+    // This looks at the actual style being applied by the CSS file
+    let currentDisplay = window.getComputedStyle(element).display;
+
+    if(element.style.display == 'block')
+    {
+        
+        element.style.display = 'none';
+    }
+    else
+    {
+        element.style.display = 'block';
+    }
+}
+
+// Goes to the defined link in the same window and tab, can't do folders well
+function gotoLink(element)
+{
+    window.location.href = element;
+}
+
+
+// Attaches the event listeners for the Menu and for this page's button for visibility
+menuDropdown.addEventListener('click', function()
+{
+    toggleVisibility(menuOutputArea);
+});
+
+// Attaches the event listeners for the Menu and for this page's button for opening links
+homeButton.addEventListener('click', function()
+{
+    gotoLink(homeLink);
+});
+aboutButton.addEventListener('click', function()
+{
+    gotoLink(aboutLink);
+});
--- index.js
+++ index.js
@@ -3,28 +3,54 @@ let menuDropdown = document.getElementById('menuDropdown');
 let menuOutputArea = document.getElementById('menuOutputArea');
 let button1 = document.getElementById('button1');
 let button1OutputArea = document.getElementById('button1OutputArea');
+let homeButton = document.getElementById('homeButton');
+let aboutButton  = document.getElementById('aboutButton');
+
+// Defining elements
+let homeLink = 'index.html';
+let aboutLink = 'about.html';
+
 
 // Shows or hides an area when a button is clicked
-function buttonClicked(areaId)
+function toggleVisibility(element)
 {
-    if(areaId.style.display == 'block')
+    // This looks at the actual style being applied by the CSS file
+    let currentDisplay = window.getComputedStyle(element).display;
+
+    if(element.style.display == 'block')
     {
         
-        areaId.style.display = 'none';
+        element.style.display = 'none';
     }
     else
     {
-        areaId.style.display = 'block';
+        element.style.display = 'block';
     }
-};
+}
+
+// Goes to the defined link in the same window and tab
+function gotoLink(element)
+{
+    window.location.href = element;
+}
 
 
-// Attaching the event listeners
+// Attaches the event listeners for the Menu and for this page's button for visibility
 menuDropdown.addEventListener('click', function()
 {
-    buttonClicked(menuOutputArea);
+    toggleVisibility(menuOutputArea);
 });
 button1.addEventListener('click', function()
 {
-    buttonClicked(button1OutputArea);
+    toggleVisibility(button1OutputArea);
+});
+
+// Attaches the event listeners for the Menu and for this page's button for opening links
+homeButton.addEventListener('click', function()
+{
+    gotoLink(homeLink);
+});
+aboutButton.addEventListener('click', function()
+{
+    gotoLink(aboutLink);
 });
